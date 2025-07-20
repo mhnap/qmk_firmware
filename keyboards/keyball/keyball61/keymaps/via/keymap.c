98b9eb1d0a85db7edfea6be9190f7f1771f215ae
Mykhailo Hnap <mike.gnap.r@gmail.com>
2025-07-20 13:20:16 +0200

    update keymap

--- keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -31,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_universal(
-    KC_F12 , KC_F1, KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10, KC_F11 ,
-    KC_TRNS, KC_NO, KC_PGUP, KC_UP  , KC_PGDN, KC_NO  ,                      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO , KC_NO  ,
-    KC_TRNS, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO  ,                      KC_NO  , KC_BTN1, KC_BTN3, KC_BTN2, KC_NO , KC_TRNS,
-    KC_NO  , KC_NO, KC_HOME, KC_NO  , KC_END , KC_NO  , KC_NO  ,    KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO , KC_TRNS,
-    KC_NO  , KC_NO, KC_NO  , KC_NO  , KC_NO  , KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_NO  , KC_NO , KC_NO
+    KC_F12 , KC_F1            , KC_F2  , KC_F3     , KC_F4  , KC_F5          ,                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10, KC_F11 ,
+    KC_TRNS, KC_NO            , KC_PGUP, KC_UP     , KC_PGDN, KC_PSCR        ,                      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO , KC_NO  ,
+    KC_TRNS, KC_KB_VOLUME_DOWN, KC_LEFT, KC_DOWN   , KC_RGHT, KC_KB_VOLUME_UP,                      KC_NO  , KC_BTN1, KC_BTN3, KC_BTN2, KC_NO , KC_TRNS,
+    KC_NO  , KC_NO            , KC_HOME, KC_KB_MUTE, KC_END , KC_NO          , KC_NO  ,    KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO , KC_TRNS,
+    KC_NO  , KC_NO            , KC_NO  , KC_NO     , KC_NO  , KC_TRNS        , KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_NO  , KC_NO , KC_NO
   ),
 
   [2] = LAYOUT_universal(
