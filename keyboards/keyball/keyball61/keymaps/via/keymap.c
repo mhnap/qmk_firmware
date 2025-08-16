09887ca63d1d0e2968a360f21c9943888e2e5954
Mykhailo Hnap <mike.gnap.r@gmail.com>
2025-08-16 15:10:13 +0200

    update keymap

--- keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -23,19 +23,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_GRV, KC_1 , KC_2 , KC_3 , KC_4 , KC_5               ,                                              KC_6                , KC_7 , KC_8   , KC_9  , KC_0   , KC_MINS             ,
-    KC_ESC, KC_Q , KC_W , KC_E , KC_R , KC_T               ,                                              KC_Y                , KC_U , KC_I   , KC_O  , KC_P   , KC_EQL              ,
-    KC_TAB, KC_A , KC_S , KC_D , KC_F , KC_G               ,                                              KC_H                , KC_J , KC_K   , KC_L  , KC_SCLN, MT(MOD_LALT,KC_QUOT),
-    KC_NO , KC_Z , KC_X , KC_C , KC_V , KC_B               , KC_LBRC            ,    KC_RBRC            , KC_N                , KC_M , KC_COMM, KC_DOT, KC_SLSH, MT(MOD_LGUI,KC_BSLS),
-    KC_NO , MO(2), KC_NO, KC_NO, KC_NO, MT(MOD_LSFT,KC_SPC), MT(MOD_LCTL,KC_DEL),    MT(MOD_RCTL,KC_ENT), MT(MOD_RSFT,KC_BSPC), MO(1), KC_NO  , KC_NO , KC_NO  , KC_NO
+    KC_GRV, KC_1 , KC_2 , KC_3 , KC_4 , KC_5               ,                                  KC_6                , KC_7         , KC_8   , KC_9  , KC_0                , KC_MINS             ,
+    KC_ESC, KC_Q , KC_W , KC_E , KC_R , KC_T               ,                                  KC_Y                , KC_U         , KC_I   , KC_O  , KC_P                , KC_EQL              ,
+    KC_TAB, KC_A , KC_S , KC_D , KC_F , KC_G               ,                                  KC_H                , KC_J         , KC_K   , KC_L  , MT(MOD_LALT,KC_SCLN), MT(MOD_LGUI,KC_QUOT),
+    KC_NO , KC_Z , KC_X , KC_C , KC_V , KC_B               , KC_LBRC,    KC_RBRC            , KC_N                , KC_M         , KC_COMM, KC_DOT, KC_SLSH             , KC_BSLS             ,
+    KC_NO , MO(2), KC_NO, KC_NO, KC_NO, MT(MOD_LSFT,KC_SPC), KC_LCTL,    MT(MOD_RCTL,KC_ENT), MT(MOD_RSFT,KC_BSPC), LT(1, KC_DEL), KC_NO  , KC_NO , KC_NO               , KC_NO
   ),
 
   [1] = LAYOUT_universal(
-    KC_F12 , KC_F1            , KC_F2  , KC_F3     , KC_F4  , KC_F5          ,                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10, KC_F11 ,
-    KC_TRNS, KC_NO            , KC_PGUP, KC_UP     , KC_PGDN, KC_PSCR        ,                      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO , KC_NO  ,
-    KC_TRNS, KC_KB_VOLUME_DOWN, KC_LEFT, KC_DOWN   , KC_RGHT, KC_KB_VOLUME_UP,                      KC_NO  , KC_BTN1, KC_BTN3, KC_BTN2, KC_NO , KC_TRNS,
-    KC_NO  , KC_NO            , KC_HOME, KC_KB_MUTE, KC_END , KC_NO          , KC_NO  ,    KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO , KC_TRNS,
-    KC_NO  , KC_NO            , KC_NO  , KC_NO     , KC_NO  , KC_TRNS        , KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_NO  , KC_NO , KC_NO
+    KC_F12 , KC_F1            , KC_F2  , KC_F3     , KC_F4  , KC_F5          ,                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
+    KC_TRNS, KC_NO            , KC_PGUP, KC_UP     , KC_PGDN, KC_PSCR        ,                      KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+    KC_TRNS, KC_KB_VOLUME_DOWN, KC_LEFT, KC_DOWN   , KC_RGHT, KC_KB_VOLUME_UP,                      KC_NO  , KC_BTN1, KC_BTN3, KC_BTN2, KC_LALT, KC_LGUI,
+    KC_NO  , KC_NO            , KC_HOME, KC_KB_MUTE, KC_END , KC_NO          , KC_NO  ,    KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+    KC_NO  , KC_NO            , KC_NO  , KC_NO     , KC_NO  , KC_TRNS        , KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_NO
   ),
 
   [2] = LAYOUT_universal(
